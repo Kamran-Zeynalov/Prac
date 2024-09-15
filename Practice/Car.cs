07597d6a1b41ad6ede5c6852e2bec919a092272b
Kamran-Zeynalov <kamraneze@code.edu.az>
2024-09-15 19:49:15 +0400

    Prac

---
+++ Practice/Car.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice
+{
+    internal class Car
+    {
+        public string Name;
+        public decimal Motor;
+        public decimal HourPower;
+        public decimal Price;
+
+
+        public Car()
+        {
+                
+        }
+
+        public Car(string name, decimal motor, decimal houtPower)
+        {
+            Name = name;
+            Motor = motor;
+            HourPower = houtPower;
+        }
+        public Car(string name, decimal price)
+        {
+            Name=name;
+            Price=price;
+        }
+    }
+}
---
+++ Practice/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Reflection.Emit;
+
+namespace Practice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Car car1 = new Car();
+            Car car2 = new Car("BMW", 120000);
+            Car car3 = new Car("Lada", 3.2m,2000);
+        }
+
+    }
+}
